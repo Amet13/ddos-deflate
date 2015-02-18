972a5af342da94830a6316790e37517effecc0b8
Amet13 <ametumerov@gmail.com>
2015-02-18 19:36:56 +0300

    big changes

--- README.md
+++ README.md
@@ -1,20 +1,40 @@
 ddos-deflate
 ============
-
 Shell script blocking DDoS attacks.
 
-Fixed configs for Debian 7 machines.
-
-
+It's work for Debian 7.
 Installation
 ------------
-Installation guide on my [blog].
-
+```bash
+# cd /tmp
+# wget https://raw.githubusercontent.com/Amet13/ddos-deflate/master/install.sh
+# chmod +x install.sh
+# ./install.sh
+```
+Add your e-mail:
+```bash
+# vim /usr/local/ddos/ddos.conf
+EMAIL_TO="mail@example.com"
+```
+Add your ignore ip's:
+```bash
+127.0.0.1
+1.1.1.1
+2.2.2.2
+```
+Add cronjob:
+```bash
+# /usr/local/ddos/ddos.sh -c
+```
+Uninstallation
+-------------
+```bash
+# chmod +x /usr/local/ddos/uninstall.sh
+# /usr/local/ddos/uninstall.sh
+```
 
 Original author
 ---------------
 [zaf@vsnl.com]
 
-
-[blog]:http://blog.amet13.name/2014/08/ddos-ddos-deflate.html
 [zaf@vsnl.com]:mailto:zaf@vsnl.com
--- install.sh
+++ install.sh
@@ -8,13 +8,13 @@ fi
 clear
 echo; echo 'Installing DOS-Deflate 0.6'; echo
 echo; echo -n 'Downloading source files...'
-wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
+wget -q -O /usr/local/ddos/ddos.conf https://raw.githubusercontent.com/Amet13/ddos-deflate/master/ddos.conf
 echo -n '.'
-wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
+wget -q -O /usr/local/ddos/LICENSE https://raw.githubusercontent.com/Amet13/ddos-deflate/master/LICENSE
 echo -n '.'
-wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
+wget -q -O /usr/local/ddos/ignore.ip.list https://raw.githubusercontent.com/Amet13/ddos-deflate/master/ignore.ip.list 
 echo -n '.'
-wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
+wget -q -O /usr/local/ddos/ddos.sh https://raw.githubusercontent.com/Amet13/ddos-deflate/master/ddos.sh 
 chmod 0755 /usr/local/ddos/ddos.sh
 cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
 echo '...done'
@@ -25,5 +25,6 @@ echo '.....done'
 echo; echo 'Installation has completed.'
 echo 'Config file is at /usr/local/ddos/ddos.conf'
 echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
+echo 'About new changes: admin@amet13.name'
 echo
 cat /usr/local/ddos/LICENSE | less
